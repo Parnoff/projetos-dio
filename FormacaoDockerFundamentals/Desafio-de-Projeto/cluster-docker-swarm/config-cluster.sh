44fb45a01a5dfcb6444fb58c6a4258763cf1f98f
Luiz Parnoff <luis.parnoff@gmail.com>
2024-03-10 20:50:50 -0300

    Removendo o token do arquivo de configuração do Docker Swarm

--- FormacaoDockerFundamentals/Desafio-de-Projeto/cluster-docker-swarm/config-cluster.sh
+++ FormacaoDockerFundamentals/Desafio-de-Projeto/cluster-docker-swarm/config-cluster.sh
@@ -3,7 +3,7 @@
 # Falta realizar uma automação neste processo, ainda estou pensando em como contornar isso.
 
 # Comando para definição do cluster Gerente
-sudo docker swarm init --advertise-addr 192.168.0.204
+sudo docker swarm init --advertise-addr <IP>
 
 # O retorno dos comando acima deverá ser executado nos nós works
-sudo docker swarm join --token SWMTKN-1-3qkgolmner3jw5p80umerr2l3gb0y3odd3hqk49by24ditgt32-au1x0t90csa5fnlmgonwa7627 192.168.0.204:2377
+sudo docker swarm join --token <TOKEN>
